ba8245b6bbfbedaacc674d3056735911680a77c6
pfree <pfree@10.16.42.86>
2019-05-13 14:24:27 -0400

    My initial commit

--- Week1/src/ca/sheridancollege/week1/softwarefundamentals/EmployeeTest.java
+++ Week1/src/ca/sheridancollege/week1/softwarefundamentals/EmployeeTest.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Line #1 Changed 
  */
 package ca.sheridancollege.week1.softwarefundamentals;
 
